b1fc38a48513fda6d9c2550235a2e9a075ed41b3
dejanjanjic <dejan.janjic@student.etf.unibl.org>
2025-08-13 09:26:55 +0200

    open health endpoints

--- api-gateway/src/main/java/eu/reportincident/api_gateway/config/RouteValidator.java
+++ api-gateway/src/main/java/eu/reportincident/api_gateway/config/RouteValidator.java
@@ -6,13 +6,21 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.function.Predicate;
 
+//curl -f http://localhost:8080/actuator/health || exit 1
+//curl -f http://localhost:8080/incident-service/actuator/health || exit 1
+//curl -f http://localhost:8080/moderation-service/actuator/health || exit 1
+//curl -f http://localhost:8080/auth-service/actuator/health || exit 1
 @Component
 public class RouteValidator {
 
     public static final List<String> openApiEndpoints = List.of(
             "/login/oauth2/code/google",
             "/api/v1/incidents/filter",
-            "/oauth2/authorization/google"
+            "/oauth2/authorization/google",
+            "/actuator/health",
+            "/incident-service/actuator/health",
+            "/moderation-service/actuator/health",
+            "/auth-service/actuator/health"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
